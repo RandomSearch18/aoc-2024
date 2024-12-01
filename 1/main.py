019a8106a3a313a3d557e117ba5bb2865ff59617
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2024-12-01 17:50:12 +0000

    to tuple

--- 1/main.py
+++ 1/main.py
@@ -1,8 +1,11 @@
-def solve() -> int:
+def solve(input_cols: list[tuple[int, int]]) -> int:
     pass
 
 
 with open("1/input.txt", "r") as file:
     data = file.read().strip()
-    input_cols = [line.split("   ") for line in data.split("\n")]
+    input_cols = []
+    for line in data.split("\n"):
+        numbers = line.split("   ")
+        input_cols.append((int(numbers[0]), int(numbers[1])))
     print(input_cols)
