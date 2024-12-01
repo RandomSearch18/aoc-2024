88a6173400460ad5e5f3d9a0a41419637dbf87ae
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2024-12-01 19:24:24 +0000

    finding smallest on left and right

--- 1/main.py
+++ 1/main.py
@@ -1,6 +1,23 @@
+from typing import Sequence, Union
+
+
+def find_smallest(numbers: list[Union[int, None]]) -> int:
+    smallest_index = 0
+    for current in range(1, len(numbers)):
+        if numbers[current] and numbers[current] < numbers[smallest_index]:  # type: ignore
+            smallest_index = current
+    return smallest_index
+
+
 def solve(input_rows: list[tuple[int, int]]) -> int:
-    left_numbers = [row[0] for row in input_rows]
-    right_numbers = [row[1] for row in input_rows]
+    left_numbers: list[Union[int, None]] = [row[0] for row in input_rows]
+    right_numbers: list[Union[int, None]] = [row[1] for row in input_rows]
+
+    count = 0
+    while count < len(left_numbers):
+        smallest_left = find_smallest(left_numbers)
+        smallest_right = find_smallest(right_numbers)
+        distance = abs(left_numbers[smallest_left] - right_numbers[smallest_right])
 
 
 with open("1/input.txt", "r") as file:
