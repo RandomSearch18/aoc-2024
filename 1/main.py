16db87fb7a0a420ecbbf874f1f1b12e458535f09
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2024-12-01 17:51:12 +0000

    rows not cols, silly

--- 1/main.py
+++ 1/main.py
@@ -1,11 +1,13 @@
-def solve(input_cols: list[tuple[int, int]]) -> int:
-    pass
+def solve(input_rows: list[tuple[int, int]]) -> int:
+    left_numbers = [row[0] for row in input_rows]
+    right_numbers = [row[1] for row in input_rows]
 
 
 with open("1/input.txt", "r") as file:
     data = file.read().strip()
-    input_cols = []
+    input_rows = []
     for line in data.split("\n"):
         numbers = line.split("   ")
-        input_cols.append((int(numbers[0]), int(numbers[1])))
-    print(input_cols)
+        input_rows.append((int(numbers[0]), int(numbers[1])))
+    # print(input_rows)
+    solve(input_rows)
