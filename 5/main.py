27a46ac61affd57af5670d12bbb559109e1b1077
RandomSearch18 <101704343+RandomSearch18@users.noreply.github.com>
2024-12-20 11:59:47 +0000

    ooooh working?

--- 5/main.py
+++ 5/main.py
@@ -20,14 +20,14 @@ def update_is_correct(rules, update):
     for i, page in enumerate(update):
         for before in find_pages_that_should_be_before(rules, page):
             if before in update:
-                # Check if the update comes before the current item
-                if not before in page[:i]:
+                # Check if the page comes before the current item
+                if not before in update[:i]:
                     print(f"{before} does not come before {page} in {update}")
                     return False
         for after in find_pages_that_should_be_after(rules, page):
             if after in update:
-                # Check if the update comes after the current item
-                if not after in page[i:]:
+                # Check if the page comes after the current item
+                if not after in update[i:]:
                     print(f"{after} does not come after {page} in {update}")
                     return False
     return True
@@ -36,10 +36,14 @@ def update_is_correct(rules, update):
 with open("5/input.txt", "r") as file:
     file_raw = file.read().strip()
     rules_raw, updates_raw = file_raw.split("\n\n")
-    rules = [(int(rule[0]), int(rule[1])) for rule in rules_raw.split("|")]
+    rules = [
+        (int(rule.split("|")[0]), int(rule.split("|")[1]))
+        for rule in rules_raw.split("\n")
+    ]
     updates = [
         [int(page) for page in update.split(",")] for update in updates_raw.split("\n")
     ]
 
     # print(rules, updates)
-    print(update_is_correct(rules, updates[3]))
+    for update in updates:
+        print(update, update_is_correct(rules, update))
