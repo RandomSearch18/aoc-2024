96c81d4b0fe52658f96e88f6079209a8fead433b
RandomSearch18 <101704343+RandomSearch18@users.noreply.github.com>
2024-12-20 11:50:51 +0000

    wrote a update_is_correct fn

--- 5/main.py
+++ 5/main.py
@@ -1,3 +1,37 @@
+def find_pages_that_should_be_after(rules, page: int):
+    result_pages = []
+    for rule in rules:
+        before, after = rule
+        if page == before:
+            result_pages.append(after)
+    return result_pages
+
+
+def find_pages_that_should_be_before(rules, page: int):
+    result_pages = []
+    for rule in rules:
+        before, after = rule
+        if page == after:
+            result_pages.append(before)
+    return result_pages
+
+
+def update_is_correct(rules, update):
+    for i, page in enumerate(update):
+        for before in find_pages_that_should_be_before(rules, page):
+            if before in update:
+                # Check if the update comes before the current item
+                if not before in page[:i]:
+                    print(f"{before} does not come before {page} in {update}")
+                    return False
+        for after in find_pages_that_should_be_after(rules, page):
+            if after in update:
+                # Check if the update comes after the current item
+                if not after in page[i:]:
+                    print(f"{after} does not come after {page} in {update}")
+                    return False
+
+
 with open("5/input.txt", "r") as file:
     file_raw = file.read().strip()
     rules_raw, updates_raw = file_raw.split("\n\n")
@@ -6,4 +40,5 @@ with open("5/input.txt", "r") as file:
         [int(page) for page in update.split(",")] for update in updates_raw.split("\n")
     ]
 
-    print(rules, updates)
+    # print(rules, updates)
+    print(update_is_correct(rules, updates[0]))
